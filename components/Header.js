30e6e311234aee3a5e5156f9edc23839cc256481
Nazile-Tag <tagnazile@gmail.com>
2022-07-26 00:22:00 -0400

    new conribute

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,14 @@
 import Image from "next/image";
 import Logo from "../static/logo.png";
 
-const styles = {};
+const styles = {
+  wrapper: 'flex justify-center gap-10 p-5 bg-[#FCC017]',
+  content: `max-w-7xl flex-1 flex justify-between gap-10`,
+  logoContainer: 'flex items-center flex-start',
+  logo: 'cursor-pointer object-contain',
+  bannerNav: 'flex cursor-pointer space-x-5 items-center',
+  accentedButton: 'bg-black text-white py-2 px-4 rounded-full',
+};
 
 const Header = () => {
   return (
@@ -11,7 +18,15 @@ const Header = () => {
             <Image 
               className={styles.logo}
               src={Logo}
+              height={40}
+              width={200}
             />
+        </div>
+        <div className={styles.bannerNav}>
+          <div>Our Story</div>
+          <div>Membership</div>
+          <div>Sign in</div>
+          <div className={styles.accentedButton}>Get Started</div>
         </div>
       </div>
     </div>
