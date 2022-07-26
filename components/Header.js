cd7c5bceeb7a290d85a0179f1aa43dd5ecba7b48
Nazile-Tag <tagnazile@gmail.com>
2022-07-25 23:00:08 -0400

    first commit

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Logo from "../static/logo.png";
+
+const styles = {};
+
+const Header = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <div className={styles.logoContainer}>
+            <Image 
+              className={styles.logo}
+              src={Logo}
+            />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
