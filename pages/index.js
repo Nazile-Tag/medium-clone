30502df26f60d22bb5570eaeaf60a86c5246fd10
Nazile-Tag <tagnazile@gmail.com>
2022-10-15 03:38:53 -0400

    continue

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,27 @@ import Banner from "../components/Banner"
 import Header from "../components/Header"
 import PostCard from "../components/PostCard"
 
+const styles = {
+  postList:`flex flex-col gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3`,
+  container:`max-w-7xl flex-1`,
+  main:`flex justify-center`,
+  wrapper:`mx-auto`
+}
+
 export default function Home() {
-  const styles = {}
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
       <Banner />
-      <PostCard />
-    </>
+      <div className={styles.main}>
+       <div className={styles.container}>
+         <div className={styles.postList}>
+          <PostCard />
+          <PostCard />
+          <PostCard />
+         </div>
+        </div>
+       </div>
+    </div>
   )
 }
